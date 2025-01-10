4027ee6ca821ff7a50772a1e663bdc27f14d8008
INovitski <INovitski@hazeltree.com>
2025-01-10 16:21:06 +0000

    fix secondary highlighting

--- lua/plugins/plug.lua
+++ lua/plugins/plug.lua
@@ -34,7 +34,7 @@ return
         config = function()
             require'eyeliner'.setup {}
             vim.api.nvim_set_hl(0, 'EyelinerPrimary', { fg='#4287f5', bold = true, underline = true,  })
-            vim.api.nvim_set_hl(0, 'EyelinerSecondary', { fg='#4287f5', underline = true })
+            vim.api.nvim_set_hl(0, 'EyelinerSecondary', { fg='#42b4f5', underline = true, italic = true  })
         end
     },
     {
