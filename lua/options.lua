0e9201fb1882852e5fc897980fc6bd96986555ec
INovitski <INovitski@hazeltree.com>
2025-01-20 19:00:54 +0000

    try different autocomplete, adjust project build

--- lua/options.lua
+++ lua/options.lua
@@ -80,7 +80,21 @@ end, { expr = true })
 local current_path = string.lower(vim.fn.getcwd())
 local htfs = "c:\\repo\\hazeltree\\main\\htfs"
 if current_path == htfs then
-    vim.opt.makeprg = 'c:\\Repo\\hazeltree\\setup\\build.bat'
+    local build_targets = { 
+        "Applications.sln",  
+        "Applications\\Hazeltree.Collateral.API\\Tests\\MarginCall.Business.IntegrationTests\\MarginCall.Business.IntegrationTests.csproj"
+    }
+    local build_target = 1
+    local current_target = build_targets[build_target]
+
+    vim.keymap.set('n', '<Leader>ct', function()
+        build_target = math.fmod(build_target, table.getn(build_targets))  + 1
+        current_target = build_targets[build_target]
+        print(build_target .. " " .. current_target)
+        vim.opt.makeprg = 'c:\\Repo\\hazeltree\\setup\\build.bat' .. ' ' .. current_target
+    end, { noremap = true })
+
+    vim.opt.makeprg = 'c:\\Repo\\hazeltree\\setup\\build.bat' .. ' ' .. current_target
     vim.opt.errorformat = '%E%f(%l\\,%c): %trror %m,%-G%.%#'
 else
     vim.cmd [[
--- lua/plugins/plug.lua
+++ lua/plugins/plug.lua
@@ -38,6 +38,8 @@ return
     },
     { 
         "Irdis/Startup",
+        -- dir = "c:\\Repo\\hazeltree\\startup\\",
+        -- dev = true,
         config = function()
             local ht = require('startup.ht')
             require("startup").setup({
@@ -143,61 +145,21 @@ return
         end,
     },
     {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "neovim/nvim-lspconfig",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "hrsh7th/cmp-vsnip",
-            "hrsh7th/vim-vsnip",
+        'saghen/blink.cmp',
+        dependencies = 'rafamadriz/friendly-snippets',
+        version = '*',
+        opts = {
+            keymap = { preset = 'default' },
+
+            appearance = {
+                use_nvim_cmp_as_default = true,
+                nerd_font_variant = 'mono'
+            },
+
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
         },
-        config = function()
-            local cmp = require('cmp')
-
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        vim.fn["vsnip#anonymous"](args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                }),
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'vsnip' }, 
-                }, {
-                    { name = 'buffer' },
-                })
-            })
-
-            cmp.setup.cmdline({ '/', '?' }, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {
-                    { name = 'buffer' }
-                }
-            })
-
-            cmp.setup.cmdline(':', {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = 'path' }
-                }, {
-                    { name = 'cmdline' }
-                }),
-                matching = { disallow_symbol_nonprefix_matching = false }
-            })
-
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            require('lspconfig')['ts_ls'].setup({
-                capabilities = capabilities
-            })
-        end
+        opts_extend = { "sources.default" }
     }
 }
