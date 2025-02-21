46c557026104c4598bac83648b63c877818c7bb9
INovitski <INovitski@hazeltree.com>
2025-02-21 19:25:05 +0000

    disable autocomplete

--- lua/plugins/plug.lua
+++ lua/plugins/plug.lua
@@ -159,24 +159,5 @@ return
                 end
             })
         end,
-    },
-    {
-        'saghen/blink.cmp',
-        dependencies = 'rafamadriz/friendly-snippets',
-        version = '*',
-        opts = {
-            keymap = { preset = 'default' },
-            appearance = {
-                use_nvim_cmp_as_default = true,
-                nerd_font_variant = 'mono'
-            },
-            cmdline = {
-                sources = {}
-            },
-            sources = {
-                default = { 'lsp', 'path', 'snippets', 'buffer' },
-            },
-        },
-        opts_extend = { "sources.default" }
     }
 }
