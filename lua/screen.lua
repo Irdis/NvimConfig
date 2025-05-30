50c82f4c7f20ae6830f2fc1124e558e3677dc67f
INovitski <INovitski@hazeltree.com>
2025-05-30 15:07:30 +0100

    spelling issues

--- lua/screen.lua
+++ lua/screen.lua
@@ -40,10 +40,9 @@ then
         "In the first age, in the first battle, when the shadows first lengthened, one stood.",
         "He chose the path of perpetual torment.",
         "In his ravenous hatred he found no peace, and with boiling blood he scoured the umbral plains, ",
-        "seeking vengence afgainst the dark lords who had robbed him.",
+        "seeking vengence against the dark lords who had robbed him.",
         "And those that tasted the bite of his sword named him...",
         "The Doom Slayer."
-
     },
     hl  = "Normal",
     opts = {
