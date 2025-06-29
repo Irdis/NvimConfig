14978c9fcfe5056a066adea1c6bfbe22f4f932cb
Ivan Novitskii <ivannovitskii@gmail.com>
2025-06-29 18:01:36 +0100

    support -i in rsqlcmd

--- lua/plugins/plug.lua
+++ lua/plugins/plug.lua
@@ -136,7 +136,7 @@ return
     },
     {
         "Irdis/RSqlCmdNvim",
-        -- dir = "C:\\Repo\\RSqlCmdNvim",
+        -- dir = "C:\\Projects\\RSqlCmdNvim",
         -- dev = true,
         config = function()
             local connection_strings = nil
@@ -161,6 +161,9 @@ return
 
             vim.keymap.set('v', '<Leader>es', ':RSqlCmd<CR>')
             vim.keymap.set('n', '<Leader>es', ':%RSqlCmd<CR>')
+
+            vim.keymap.set('v', '<Leader>eS', ':RSqlCmd -i<CR>')
+            vim.keymap.set('n', '<Leader>eS', ':%RSqlCmd -i<CR>')
         end
     },
     {
