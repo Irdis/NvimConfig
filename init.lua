a1d29c79451832486d396ca600cf9f5a7f1b044a
INovitski <INovitski@hazeltree.com>
2025-07-08 11:38:23 +0100

    factor path

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("config.lazy")
 require("options")
 require("workspace")
+require("ext/follow_path")
---
+++ lua/ext/follow_path.lua
@@ -0,0 +1,40 @@
+local function follow_path(precise)
+    local pos = vim.api.nvim_win_get_cursor(0)
+    local col = pos[2]
+    local line = vim.api.nvim_get_current_line()
+
+    local b = nil
+    local e = nil
+    local path = ""
+    local lineno = ""
+    local found = false
+
+    b, e = 0, 0
+    while true do
+        b, e, path, lineno = string.find(line, "([%w%._%-/\\:]+%.%w+):line (%d+)", b + 1)
+        if b == nil or e == nil then break end
+        if not precise then
+            found = true
+            break
+        end
+        if b <= col and col <= e then
+            found = true
+            break
+        end
+        if b > col then break end
+        b = e
+    end
+    if found then
+        vim.cmd("edit " .. vim.fn.fnameescape(path))
+        vim.api.nvim_win_set_cursor(0, {tonumber(lineno), 0})
+        vim.cmd("zz")
+    else
+        print('Nothing has found')
+    end
+end
+vim.keymap.set('n', '<Leader>fp',function()
+    follow_path(false)
+end, { noremap = true })
+vim.keymap.set('n', '<Leader>fP',function()
+    follow_path(true)
+end, { noremap = true })
