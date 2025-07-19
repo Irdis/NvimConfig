4ee864b3f9ed65c0ba910514673a8759f4203afc
Ivan Novitskii <ivannovitskii@gmail.com>
2025-07-19 19:54:36 +0100

    alter tab buf switch

--- lua/options.lua
+++ lua/options.lua
@@ -83,7 +83,7 @@ vim.keymap.set('n', '<Leader>gl', ':diffget \\2<CR>')
 vim.keymap.set('n', '<Leader>gr', ':diffget \\3<CR>')
 
 vim.keymap.set('n', '<C-q>', ':bnext<CR>')
-vim.keymap.set('n', '<C-a>', ':bprev<CR>')
+vim.keymap.set('n', '<C-z>', ':bprev<CR>')
 vim.keymap.set('n', '<C-x>', ':bd<CR>')
 
 vim.keymap.set('n', '<Leader>ld', ':G log -2000 --all --decorate --oneline --graph --pretty=\'%h ~> %aN %as %ar%d ~> %B%-C()\'<CR>')
