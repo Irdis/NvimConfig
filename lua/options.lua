57a89bb10a1cf8164e27dd659542f582b9581f49
INovitski <INovitski@hazeltree.com>
2025-07-22 15:58:37 +0100

    not used

--- lua/options.lua
+++ lua/options.lua
@@ -88,10 +88,6 @@ vim.keymap.set('n', '<Leader>el', ':%lua<CR>')
 vim.keymap.set('n', '<Leader>gl', ':diffget \\2<CR>')
 vim.keymap.set('n', '<Leader>gr', ':diffget \\3<CR>')
 
-vim.keymap.set('n', '<C-q>', ':bnext<CR>')
-vim.keymap.set('n', '<C-z>', ':bprev<CR>')
-vim.keymap.set('n', '<C-x>', ':bd<CR>')
-
 vim.keymap.set('n', '<Leader>ld', ':G log -2000 --all --decorate --oneline --graph --pretty=\'%h ~> %aN %as %ar%d ~> %B%-C()\'<CR>')
 vim.keymap.set('n', '<Leader>lf', ':G log -2000 --all --decorate --oneline --graph --first-parent --pretty=\'%h ~> %aN %as %ar%d ~> %B%-C()\'<CR>')
 vim.keymap.set('n', '<Leader>ll', ':r !git log -1 --pretty=\\%B\\%-C() --author=Novit<CR>')
