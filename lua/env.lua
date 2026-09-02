3af88ce0644b71ca13c0df8379c3e4c162e85f20
Ivan Novitski <inovitski@hazeltree.com>
2026-09-02 11:22:28 +0100

    fix work

--- lua/env.lua
+++ lua/env.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.at_work = function()
     local lines = vim.fn.systemlist('hostname');
-    return lines[1]:find('BNM01') ~= nil
+    return lines[1]:find('BNM') ~= nil
 end
 
 M.is_linux = function()
